f3c471cbeecb7ec74b9baed3f006ef253c3b33b1
Mark Gire <magire01@gmail.com>
2020-08-18 02:14:55 -0400

    New workout can be added

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const db = require("../model");
+
+require("../config/connection");
+
+
+module.exports = function(app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}, (err, data) => {
+            if(err) {
+                res.send(err)
+            } else {
+                console.log("All Workout test: ", data);
+                res.json(data);
+            }
+        });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+            .then(data => {
+                console.log(data);
+                res.json(data);
+            })
+            .catch(err => {
+                    res.json(err)
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+                req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true }
+            )
+            .then(data => {
+                console.log(data);
+                res.json(data);
+            })
+            .catch(err => res.json(err));
+    });
+}
